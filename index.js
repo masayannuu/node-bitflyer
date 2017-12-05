8e3537ea8c99e0fd9410e737a3c87eb88fdc4285
masayannuu <masayannuu@gmail.com>
2017-12-06 00:00:14 +0900

    :sparkles:add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const RestClient = require('./lib/RestClient')
+const RealtimeClient = require('./lib/RealtimeClient')
+
+module.exports = {
+    RestClient: RestClient
+    RealtimeClient: RealtimeClient,
+}
